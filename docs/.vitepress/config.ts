66cf17fe39ea57c9661762537fdbf6a48573124c
李江辰 <SpaceCode@foxmail.com>
2023-03-07 16:28:03 +0800

    feat: 新增官方文档基础框架

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,9 +1,33 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
-  title: 'ChineseLunar',
+  title: 'CLunar',
   themeConfig: {
-    logo: '/public/t1.png',
-    siteTitle: '中国农历',
+    logo: '/public/logo.png',
+    siteTitle: 'CLunar官方中文文档',
+
+    nav: [{ text: '文档', link: '/guide/installation' }],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/LIjiAngChen8/chinese-lunar' },
+    ],
+
+    sidebar: {
+      '/guild/': [
+        {
+          text: '基础',
+          items: [
+            {
+              text: '安装',
+              link: '/guild/installation',
+            },
+            {
+              text: '快速开始',
+              link: '/guild/quickstart',
+            },
+          ],
+        },
+      ],
+    },
   },
 });
--- docs/index.md
+++ docs/index.md
@@ -4,7 +4,7 @@ layout: home
 hero:
   name: ChineseLunar
   text: 农历(阴阳合历)
-  tagline: 一款超轻量的中国农历工具，快捷的转换公农历。提供星座、儒略日、干支、生肖、节气、节日等API。🔧
+  tagline: CLunar是一个轻量的处理农历日期的 JavaScript 库，支持星座、干支、生肖、节气、节日等API。
   image:
     src: /public/logo.png
     alt: lunar
@@ -13,4 +13,18 @@ hero:
       text: 快速开始
     - theme: alt
       text: GitHub
+      link: https://github.com/LIjiAngChen8/chinese-lunar
+features:
+  - icon: ⚡️
+    title: 超轻量方案
+    details: 仅 3kb 大小库却拥有丰富又强大的API。
+  - icon: 🔌
+    title: 可扩展
+    details: 支持链式操作，可根据自己的需求进行扩展。
+  - icon: 🔩
+    title: 支持多种环境
+    details: 支持CommonJS、AMD、UMD等多格式模块化规范。
+  - icon: ⏰
+    title: dayjs
+    details: 与dayjs、Moment.js无缝搭配。
 ---
